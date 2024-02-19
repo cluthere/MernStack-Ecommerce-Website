040c9a95694dfc35c12b4b32ee6923b8a102c466
cluthere <69466884+cluthere/@users.noreply.github.com>
2024-02-20 02:06:08 +0530

    added file in db index.js

--- ecomBackend/model/user.model.js
+++ ecomBackend/model/user.model.js
@@ -1,2 +1,36 @@
 import mongoose from 'mongoose'
 
+const userSchema = mongoose.Schema({
+    userName:{
+        type:String,
+        required:[true, "please provide userName"]
+    },
+    FullName:{
+        type:String,
+        required:[true, "please provide your full name"]
+    },
+    Avatar:{
+        type:String,
+        required:[true, "please provide your avatar"]
+    },
+    Phone:{
+        type:String,
+        required:[true, "Please provide your active Phone number"]
+    },
+    password:{
+        type:String,
+        required:[true, "Please provide password"]
+    },
+    email:{
+        type:String,
+        required:[true, "Please provide your email address"]
+    },
+    address:{
+        type:String,
+    }
+
+},{timesmaps: true})
+
+const User = mongoose.model(User, userSchema);
+
+export default User;
--- ecomFrontend/src/components/homePage/slide/Images.jsx
+++ ecomFrontend/src/components/homePage/slide/Images.jsx
@@ -1,12 +1,15 @@
 import Slider from './Slider';
 
-import p1 from "../../assets/p1.jpg"
-import p2 from "../../assets/p2.jpg"
-import p3 from "../../assets/p3.jpg"
-import p4 from "../../assets/p4.jpg"
+const apple = "https://www.gaincity.com/media/catalog/category/SEA_iPhone_15_Pro_Sep23_Web_Banner_Pre-Avail_1280x457_FFH-BuyNow_2.jpg"
+const google = "https://www.electronicsonedge.com/assets/components/phpthumbof/cache/google-banner_1.35388a4f93748edd2897aed6c80ec562.jpg"
+const samsung = "https://dev.myg.in/images/companies/1/prebook-landing-page-banner.jpg?1705583967988"
+const realme = "https://image01.realme.net/general/20230608/1686210624428871072fcb5294ae0a771d712fe3a4ca4.jpg.webp?width=2280&height=900&size=318592"
+const oppo = "https://assorted.downloads.oppo.com/static/archives/images/dd/Smartphones/A92/A92-white-top%20banner-PC.jpg" 
+const redmi = "https://etimg.etb2bimg.com/photo/96783408.cms"
+
 
 export default function Images(){
-  const images = [p2, p3, p4];
+  const images = [apple, google, samsung, realme, oppo, redmi];
 
   return (
     // <div className=" flex ">
