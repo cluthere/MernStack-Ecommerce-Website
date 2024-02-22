69c27c9395b2d02f2192abb37edb36482ad79ca1
cluthere <69466884+cluthere/@users.noreply.github.com>
2024-02-22 22:56:56 +0530

    product schema done for order, product, category

--- ecomBackend/model/user.model.js
+++ ecomBackend/model/user.model.js
@@ -35,6 +35,6 @@ const userSchema = new mongoose.Schema({
 
 },{timesmaps: true})
 
-const User = mongoose.model(User, userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
